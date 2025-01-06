4753318d6005a883b13b5f007bd0ea3887432489
yalh76 <yalh@yahoo.com>
2025-01-06 06:56:06 +0100

    First commit

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,7 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+ynh_user="${app}_notifs"
+
+python_version="3.12"
